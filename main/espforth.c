387675f93c2e6dc3ab0698caea9d55c4e4d2e41c
Brad Nelson <flagxor@gmail.com>
2020-06-27 13:51:23 -0700

    Revised to build with latest SDK

--- main/espforth.c
+++ main/espforth.c
@@ -501,9 +501,9 @@ static void Init(void) {
 
   setvbuf(stdin, NULL, _IONBF, 0);
   setvbuf(stdout, NULL, _IONBF, 0);
-  ESP_ERROR_CHECK( uart_driver_install( (uart_port_t)CONFIG_CONSOLE_UART_NUM,
+  ESP_ERROR_CHECK( uart_driver_install( (uart_port_t)CONFIG_ESP_CONSOLE_UART_NUM,
           256, 0, 0, NULL, 0) );
-  esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);
+  esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
   esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
   esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
 
