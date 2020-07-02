230eebeeafc1b2c399b856b8e0da374eb64b4c15
Brad Nelson <flagxor@gmail.com>
2020-07-01 18:22:49 -0700

    Fix does>

--- main/boot.fs
+++ main/boot.fs
@@ -1,7 +1,17 @@
 ( Things loaded at boot )
 : test 0 100 for dup . 1+ next drop cr ;
 
+64 ' create >name 1+ c!  ( hide create )
+' dolist @ constant op_dolist
+: n,   compile dolit , ;
+: [']   ' n, ; immediate
 : docreate   r> ;
-: create   : [ ' docreate ] literal , [ ' [ , ] overt ;
-: does>   here last @ name> cell+ ! [ ' dolist @ ] literal , [ ' r> ] literal , ] ;
-
+: create   : compile docreate [compile] [ overt ;
+: hookupdoes   last @ name> cell+ ! ;
+: does>   'eval @ ['] $interpret = if
+      here hookupdoes
+   else
+      here 4 cells + n, compile hookupdoes compile exit
+   then
+   op_dolist , compile r> [compile] ]
+; immediate
