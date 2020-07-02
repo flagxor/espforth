37c0286571a3878507b330e31626dcfd6930e12d
Brad Nelson <flagxor@gmail.com>
2020-07-01 17:35:55 -0700

    Add does> sorta

--- main/boot.fs
+++ main/boot.fs
@@ -1,3 +1,7 @@
 ( Things loaded at boot )
 : test 0 100 for dup . 1+ next drop cr ;
 
+: docreate   r> ;
+: create   : [ ' docreate ] literal , [ ' [ , ] overt ;
+: does>   here last @ name> cell+ ! [ ' dolist @ ] literal , [ ' r> ] literal , ] ;
+
